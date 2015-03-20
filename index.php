b037616c987ce025002d736ee35dee46a7f760c6
Ryan Pace Sloan <ryansdeal@hotmail.com>
2015-03-20 17:10:39 -0600

    commiting

--- index.php
+++ index.php
@@ -39,67 +39,107 @@
 	</style>
 </head>
 <body>
-<main>
 <header>
-	<div class="panel panel-default">
-		<div class="panel-body")">
-		<span style="color: black; font-size: .7em">&starf;</span> Ryan Pace Sloan
-		</div>
-	</div>
-</header>
-<section>
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="# "><span class="glyphicon glyphicon-cloud"
+																		 aria-hidden="true"></span> Ryan Pace Sloan</a>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li></li>
+						<li><a href="#"></a></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li><div class="btn-group" role="group">
+									<button style="height: 4em; width: 16em" type="button" class="btn btn-default
+								dropdown-toggle"
+								data-toggle="dropdown"
+											  aria-expanded="false">
+										Web Dev Portfolio <span style="font-size: 1.5em">&circledast;</span>
+										<span class="caret"></span>
+									</button>
+									<ul class="dropdown-menu" role="menu">
+										<li><a href="http://www.easyconversiontools.com">Easy Conversion Tools</a></li>
+										<li><a href="http://www.unitydictionary.info">Unity Turkish English Dictionary</a></li>
+										<li><a href="http://www.przm-air.com">PRZM Air</a></li>
+									</ul>
+								</div></li>
+
+
+					</ul>
+
+					<ul class="nav navbar-nav navbar-right">
+						<li class="disabled"> </li>
+						<li class="active">
+							<div class="btn-group" role="group">
+								<button style="height: 4em; width: 16em" type="button" class="btn btn-default
+								dropdown-toggle" data-toggle="dropdown"
+										  aria-expanded="false">Tweets @ryanpacesloan <span class="caret"></span>
+								</button>
+								<ul class="dropdown-menu" role="menu" aria-labelledby="tweetDrop">
+									<li role="presentation"><a role="menuitem" tabindex="-1" class="twitter-timeline"
+																		href="https://twitter.com/ryanpacesloan"
+																		data-widget-id="536299887302701056"></a>
+										<script>
+											!function(d,s,id){
+												var js,
+													fjs=d.getElementsByTagName(s)[0],
+													p=/^http:/.test(d.location)?'http':'https';
+												if(!d.getElementById(id))
+												{
+													js=d.createElement(s);
+													js.id=id;
+													js.src=p+"://platform.twitter.com/widgets.js";
+													fjs.parentNode.insertBefore(js,fjs);
+												}
+											}
+											(document,"script","twitter-wjs");
+										</script>
+									</li>
+								</ul>
+							</div>
+						</li>
+						<li></li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div><!-- /.container-fluid -->
+		</nav>
+	</header>
+<main>
 	<article>
-	<div class="btn-group-vertical" role="group" aria-label="rps">
-		<div class="btn-group" role="group">
-			<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-				Web Dev Portfolio <span style="font-size: 1.5em">&circledast;</span>
-				<span class="caret"></span>
-			</button>
-			<ul class="dropdown-menu" role="menu">
-				<li><a href="http://www.easyconversiontools.com">Easy Conversion Tools</a></li>
-				<li><a href="http://www.unitydictionary.info">Unity Turkish English Dictionary</a></li>
-				<li><a href="http://www.przm-air.com">PRZM Air</a></li>
-			</ul>
+		<div class="icon-list">
+			<a><span class="glyphicon glyphicon-star"
+							aria-hidden="true"></span><span class="glyphicon glyphicon-star"
+																	  aria-hidden="true"></span><span class="glyphicon glyphicon-star"
+																												 aria-hidden="true"></span></a><br>
+			<a href="http://www.github.com/ryanpsloan" target="_blank">Github Repository Link</a><br>
+			<a href="https://www.linkedin.com/profile/view?id=85723107&trk=spm_pic" target="_blank">Linked In Profile</a><br>
+			<a href="resume.html">Resume</a><br>
+			<a href="contact.php">Contact</a><br>
+			<a href="location.php">Location</a><br>
+			<a href="originalSite/original.html" target="_blank">Fun Site</a><br>
+			<a href="#" onclick=window.open("http://www.ryanpacesloan.com/originalSite/windowLinks.html")>
+				Developed Sites</a><br>
+			<a><span class="glyphicon glyphicon-star"
+						aria-hidden="true"></span><span class="glyphicon glyphicon-star"
+																  aria-hidden="true"></span><span class="glyphicon glyphicon-star"
+																											 aria-hidden="true"></span></a><br>
+
+
 		</div>
-		<div class="btn-group" role="group">
-			<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"
-				aria-expanded="false">Tweets @ryanpacesloan <span class="caret"></span>
-			</button>
-			<ul class="dropdown-menu" role="menu" aria-labelledby="tweetDrop">
-				<li role="presentation"><a role="menuitem" tabindex="-1" class="twitter-timeline"
-													href="https://twitter.com/ryanpacesloan"
-													data-widget-id="536299887302701056"></a>
-					<script>
-						!function(d,s,id){
-							var js,
-								fjs=d.getElementsByTagName(s)[0],
-								p=/^http:/.test(d.location)?'http':'https';
-							if(!d.getElementById(id))
-							{
-								js=d.createElement(s);
-								js.id=id;
-								js.src=p+"://platform.twitter.com/widgets.js";
-								fjs.parentNode.insertBefore(js,fjs);
-							}
-						}
-						(document,"script","twitter-wjs");
-					</script>
-				</li>
-			</ul>
-		</div>
-	</div>
 	</article>
-	<div class="icon-list">
-		<a href="http://www.github.com/ryanpsloan" target="_blank">Github Repository Link</a><br>
-		<a href="https://www.linkedin.com/profile/view?id=85723107&trk=spm_pic" target="_blank">Linked In Profile</a><br>
-		<a href="resume.html">Resume</a><br>
-		<a href="contact.php">Contact</a><br>
-		<a href="location.php">Location</a><br>
-		<a href="originalSite/original.html" target="_blank">Fun Site</a><br>
-
-	</div>
-
-</section>
 </main>
 <footer>
 
